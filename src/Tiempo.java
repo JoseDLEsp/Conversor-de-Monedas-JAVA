60b9920a8862b511de282fcab635856d04848f2e
José <josedlesp@gmail.com>
2023-03-10 22:38:51 -0600

    V1.0 El convertidor de tiempo funciona correctamente

--- src/Tiempo.java
+++ src/Tiempo.java
@@ -2,7 +2,7 @@
 public class Tiempo extends Conversor {
 	
 	public String [] listaTiempos = new String [] {
-			"Segundos","Minutos","Horas", "Dias", "Semanas", "Meses", "Anhos"};
+			"Seconds","Minutes","Hours", "Days", "Weeks", "Months", "Years"};
 	
 	public String[] getListaOpciones() {
 		return listaTiempos;
@@ -13,9 +13,15 @@ public class Tiempo extends Conversor {
 	}
 
 	@Override
-	public double convertirValores(double valor,String tipo1, String tipo2, int index1, int index2) {		
-		return 0;
+	public double convertirValores(double valor,String tipo1, String tipo2, int index1, int index2) {			
+		double [] equivalenciaSeg = new double [] {1, 60, 3600, 86400, 604800, 2629800, 31557600};
+		
+		return ((valor*equivalenciaSeg[index1])/equivalenciaSeg[index2]);
+		
+		
 	}
 
+	
+	
 	
 }
